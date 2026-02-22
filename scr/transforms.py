76c5a58a1b2187d027dfbf1bfe5154ba266ec449
aalarina <147263709+aalarina@users.noreply.github.com>
2026-02-22 19:32:40 +0200

    Delete scr/transforms.py

--- scr/transforms.py
+++
@@ -1,9 +0,0 @@
-from torchvision import transforms
-
-def get_transforms():
-    return transforms.Compose([
-        transforms.Resize((224, 224)),
-        transforms.RandomHorizontalFlip(),
-        transforms.RandomRotation(15),
-        transforms.ToTensor()
-    ]) 
