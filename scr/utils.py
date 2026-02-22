52ee2b9eed8066eb24fe070d70b8590846e2e73d
aalarina <147263709+aalarina@users.noreply.github.com>
2026-02-22 14:17:45 +0200

    Create utils.py

---
+++ scr/utils.py
@@ -0,0 +1,34 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from torch.utils.data import DataLoader
+
+# Visualization helpers
+
+def show_augmented_examples(dataset, num=8):
+  # show some images from dataset (assumes transform includes ToTensor & normalization)
+  loader = DataLoader(dataset, batch_size=num, shuffle=True)
+  imgs, labels = next(iter(loader))
+  imgs = imgs.numpy()
+  imgs = imgs.transpose(0,2,3,1)
+
+  # unnormalize
+  mean = np.array([0.485, 0.456, 0.406])
+  std = np.array([0.229, 0.224, 0.225])
+  imgs = std * imgs + mean
+  imgs = np.clip(imgs, 0, 1)
+
+  plt.figure(figsize=(12,4))
+  for i in range(min(num, imgs.shape[0])):
+    plt.subplot(1, num, i+1)
+    plt.imshow(imgs[i])
+    plt.title(str(int(labels[i].item())))
+    plt.axis('off')
+  plt.show()
+
+def count_labels(dataset):
+    labels = [label for _, label in dataset]
+    unique, counts = np.unique(labels, return_counts=True)
+    return dict(zip(unique, counts))
+
+print("Train label distribution:", count_labels(train_dataset))
+print("Val label distribution:", count_labels(val_dataset))
