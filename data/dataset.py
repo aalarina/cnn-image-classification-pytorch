39b6fb1e3c0148e06ca8a0e49139b1398e17e7bc
aalarina <147263709+aalarina@users.noreply.github.com>
2026-02-22 13:39:43 +0200

    Create dataset.py

---
+++ data/dataset.py
@@ -0,0 +1,19 @@
+import torch
+from torch.utils.data import Dataset
+from PIL import Image
+
+class ImageDataset(Dataset):
+    def __init__(self, image_paths, labels, transform=None):
+        self.image_paths = image_paths
+        self.labels = labels
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.image_paths)
+
+    def __getitem__(self, idx):
+        img = Image.open(self.image_paths[idx]).convert("RGB")
+        if self.transform:
+            img = self.transform(img)
+        label = self.labels[idx]
+        return img, label
